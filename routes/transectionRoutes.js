d505b43acc6a3ef2825de1c2c2d20b82cceeed65
BharatiWagh <114480738+BharatiWagh@users.noreply.github.com>
2023-06-05 09:59:42 +0530

    code added

---
+++ routes/transectionRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+    addTransection,
+    getAllTransection,
+    editTransection,
+    deleteTransection
+} = require("../controllers/transectionCtrl");
+
+
+//router object
+const router = express.Router();
+//addd transection post method
+router.post('/add-transection', addTransection);
+//edit transection post method
+router.post('/edit-transection', editTransection);
+//delete transection post method
+router.post('/delete-transection', deleteTransection);
+
+
+//get transections 
+router.post('/get-transection', getAllTransection);
+
+
+module.exports = router;
